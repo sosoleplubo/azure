f7a5793c1b50dd8d6393ccf4bb49d368e4310c88
Sofiane <sofianefugon@icloud.com>
2025-05-15 12:50:48 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Utilisateurs</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }} - {{ user.email }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/api/users')
+        .then(response => response.json())
+        .then(data => {
+          this.users = data;
+        });
+  }
+}
+</script>
